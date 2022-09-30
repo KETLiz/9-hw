e445e7c821be4d2992c0450395064b1fee5cba65
KET <varlamova_e.t@mail.ru>
2022-09-30 19:24:18 +0300

    3ttt

--- 3 t/Program.cs
+++ 3 t/Program.cs
@@ -11,8 +11,8 @@ int n = Convert.ToInt32(Console.ReadLine());
 int Akkerman(int m1, int n1)
 {
     if (m==0) return n+ 1;
-    if (m > 0 && n == 0) return Akkerman(m - 1, 1);
-    if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    else if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    else (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
 }
 int x = Akkerman(m, n);
 Console.Write(x);
